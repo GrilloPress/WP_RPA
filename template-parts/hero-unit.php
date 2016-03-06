c6d986a340b82dd456f1c6edf10e1d1db1bed7a4
GrilloPress <grillopress@gmail.com>
2016-03-06 23:06:25 +0000

    add hero unit buttons and images

--- template-parts/hero-unit.php
+++ template-parts/hero-unit.php
@@ -1,4 +1,38 @@
 
 <div class="jumbotron">
-  <img src="<?php echo get_template_directory_uri() . '/images/radiation-header-full.jpg' ;?>" alt="" class="img-responsive hidden-xs">
+  <img src="<?php echo get_template_directory_uri() . '/images/radiation-header-full.jpg' ;?>" alt="<?php echo get_bloginfo('description'); ?>" class="img-responsive hidden-xs">
+  <img src="<?php echo get_template_directory_uri() . '/images/logo-doc.png' ;?>" alt="<?php echo get_bloginfo('description'); ?>" class="img-responsive visible-xs mobile-header">
+  
+    <section class="nav-block-container">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 nav-block">
+          <a href="<?php echo home_url('/our-services/#top'); ?>">
+            <img width="64" src="<?php echo get_template_directory_uri() . '/images/circle156.png' ;?>">  
+            <h5>Our Services</h5>
+            </a>
+        </div>
+
+        <div class="col-md-3 col-sm-6 nav-block">
+          <a href="<?php echo home_url('/our-team/#top'); ?>">
+            <img width="64" src="<?php echo get_template_directory_uri() . '/images/multiple25.png' ;?>">  
+            <h5>Our Team</h5>
+            </a>
+        </div>
+
+        <div class="col-md-3 col-sm-6 nav-block">
+          <a href="<?php echo home_url('/#testimonials'); ?>">
+            <img width="64" src="<?php echo get_template_directory_uri() . '/images/quotation_white.png' ;?>">  
+            <h5>Testimonials</h5>
+          </a>
+        </div>
+        
+        <div class="col-md-3 col-sm-6 nav-block">
+          <a href="<?php echo home_url('/conact-us/'); ?>">
+            <img width="64" src="<?php echo get_template_directory_uri() . '/images/email19.png' ;?>">  
+            <h5>Contact us</h5>
+          </a>
+        </div>
+    </div>  
+  </section>
+  
 </div>
