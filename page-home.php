e4048ef73eb099ad7afca2b8ec5cb5447f331780
GrilloPress <grillopress@gmail.com>
2016-03-07 22:40:50 +0000

    add team section to page-home.php template

--- page-home.php
+++ page-home.php
@@ -19,22 +19,116 @@ get_header(); ?>
      </section>
      
      
-    <div class="row">
-      <main id="main" class="col-md-7 col-md-offset-1 col-sm-8" role="main">
+     <?php get_template_part('template-parts/service', 'listing') ;?>
+     
+<div class="row">
+  <div class="col-md-12">
+    <div class="team-block">
+      <h2 class="text-center">Our Team</h2>
+    </div>
+  </div>
+  
+  <div class="col-md-5 col-md-offset-1">
+    <p class="lead">The quality of RPA services is assured by our highly qualified and experienced team of UK Health Care Professions Council Registered Clinical Scientists and Technologists.</p>
+    <p>The team comprises expert Radiation Protection Advisers and Radioactive Waste Advisors certificated by RPA2000 and Medical Physics Experts.</p>
+    <p>This skilled team is supported by our extensive facilities for equipment performance measurement, image analysis, personnel dosimetry, sample analysis and monitoring equipment.</p>
+    <p>We also use a range of up-to-date commercial and bespoke analysis software. As a Section we are part of one of the largest Medical Physics and Clinical Engineering Departments within one of the leading NHS Teaching Hospitals in the UK.</p>
+    <p>Through our services, clients have access to specialist electronic, mechanical and software engineering, design and fabrication facilities, enabling us to fully support a comprehensive range of service requirements.</p>
+  
+    <a class="btn btn-lg btn-block btn-primary" href="<?php echo home_url('/our-team/'); ?>">Meet the Team</a>
+  </div>
+  
+  <div class="col-md-4 col-md-offset-1 hidden-xs">
+    <div class="spacer">
+        <div class="row">
 
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/giles-morrison.jpg' ;?>">
+              </section>
+            </div>
 
-			<?php endwhile; // End of the loop. ?>
 
-		  </main><!-- #main -->
-      
-      <aside class="col-md-3 col-sm-4">
-        <?php get_sidebar(); ?>
-      </aside>
-      
-	  </div><!-- #primary -->
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/mark-singleton.jpg' ;?>">
+              </section>
+            </div>
+
+
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/paul-harris.jpg' ;?>">
+              </section>
+            </div>
+
+
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/chris-ogley.jpg' ;?>">
+              </section>
+            </div>
+
+
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/tracy-soanes.jpg' ;?>">
+              </section>
+            </div>
+
+
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/prashant-verma.jpg' ;?>">
+              </section>
+            </div>
+
+
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/matt-hill.jpg' ;?>">
+              </section>
+            </div>
+
+
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/mike-tyas.jpg' ;?>">
+              </section>
+            </div>
+
+
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/mark-williams.jpg' ;?>">
+              </section>
+            </div>
+
+
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/andy-fry.jpg' ;?>">
+              </section>
+            </div>
+
+
+            <div class="col-md-4 col-sm-4">
+              <section class="card-team-member">
+                <img class="img-responsive" width="100%" src="<?php echo get_template_directory_uri() . '/images/team/wendy-fish.jpg' ;?>">
+              </section>
+            </div>
+
+
+      </div>
+    </div>
+  </div>
+  
+</div>
+     
+     
+     
+     
   </div>
 
 <?php get_footer(); ?>
---
+++ template-parts/service-listing.php
@@ -0,0 +1,207 @@
+<div class="row">
+  <div class="col-md-12" id="our-services">
+      <h2 class="section-header">Our Services</h2>
+  </div>
+    
+    <div class="col-md-5 col-md-offset-1">
+      <section class="service-text">
+        <p class="lead">Our Radiation Protection Assurance (RPA) service offers our clients comprehensive and high quality radiation protection services that are tailored to their individual needs.</p>
+        <p>We deliver corporate assurance to our clients through advice which is both proactive and responsive but proportionate and relevant to their service requirements, providing radiation protection advice to hospitals, healthcare services and industry on:</p>
+        <ul>
+          <li>Healthcare Imaging</li>
+          <li>Sealed and unsealed radioactive sources</li>
+          <li>Therapy Treatment (External Beam Radiotherapy and Nuclear Medicine Therapy)</li>
+          <li>Clinical Research</li>
+          <li>Disposal of radioactive clinical waste</li>
+          <li>Radiation Protection Design</li>
+        </ul>
+       <p>We also provide Quality Assurance (QA) advice for users of these modalities:</p>
+        <ul>
+          <li>X-rays</li>
+          <li>Sealed and Unsealed radioactive sources </li>
+          <li>External Beam or Brachytherapy Radiotherapy</li>
+          <li>Magnetic Resonance Imaging (MRI)</li>
+          <li>Ultrasound</li>
+          <li>Lasers</li>
+          <li>Ultraviolet light</li>
+        </ul>
+        <p>As part of one of the largest Medical Physics and Clinical Engineering Departments within one of the leading NHS Teaching Hospitals in the UK, we have have access to specialist electronic, mechanical and software engineering design and fabrication facilities, enabling us to fully support all of our client’s service requirements.</p>
+        <p>For more information about our services, see our <a href="our-services.html#service-index">detailed service listing</a> or get in touch with out team via our online form.</p>
+      </section>
+    </div>
+    
+    <div class="col-md-4 col-md-offset-1">
+      
+     <form id="service-request-form">
+      <div class="form-group">
+        <label for="customerName">Full name <span class="required">(required)</span></label>
+      <input type="text" class="form-control" id="customerName" placeholder="Full Name" required="">
+      </div>
+      <div class="form-group">
+        <label for="jobTitle">Job Title <span class="required">(required)</span></label>
+      <input type="text" class="form-control" id="jobTitle" placeholder="Job Title" required="">
+      </div>
+      <div class="form-group">
+        <label for="customerCompany">Organisation <span class="required">(required)</span></label>
+      <input type="text" class="form-control" id="customerCompany" placeholder="Organization" required="">
+      </div>
+      <div class="form-group">
+        <label for="phoneNumber">Phone Number <span class="required">(required)</span></label>
+      <input type="tel" class="form-control" id="phoneNumber" placeholder="Phone Number" required="">
+      </div>
+      <div class="form-group">
+        <label for="customerEmail">Email address <span class="required">(required)</span></label>
+        <input type="email" class="form-control" id="customerEmail" placeholder="Email" required="">
+      </div>
+      <div class="form-group">
+        <label for="servicesRequired">Service Requested</label>
+        <select class="form-control" id="servicesRequired">
+        <option>Radiation Protection</option>
+        <option>Service Design &amp; Consultation</option>
+        <option>Quality Assurance</option>
+        <option>Corporate Assurance</option>
+        <option>Project Management</option>
+        <option>Training &amp; Staff Development</option>
+        <option>Ethics Guidance</option>
+        <option>Other</option>
+      </select>
+      </div>
+      <div class="form-group">
+        <label for="customerMessage">Requirements &amp; Further Information</label>
+        <textarea class="form-control" id="customerMessage" rows="3"></textarea>
+      </div>
+      <button type="submit" class="btn btn-lg btn-block btn-info">Get in touch</button>
+    </form>
+    </div>
+    
+    
+    <div class="col-md-12">
+      
+      <section id="featured-services">
+        <div class="row">
+      
+          <div class="col-md-3 col-sm-6">
+             <div class="media">
+              <div class="media-left">
+                <a href="#" data-toggle="modal" data-target="#myModal">
+                  <img width="84" src="<?php echo get_template_directory_uri() . '/images/png/signpost3.png' ;?>">  
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Service Design &amp; Consultation</h4>
+                <p>From consulting on healthcare imaging and therapy treatment to radiation protection service design, our radiation protection team can help provide the service you need</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-6">
+             <div class="media">
+              <div class="media-left">
+                <a href="#" data-toggle="modal" data-target="#myModal">
+                 <img width="84" src="<?php echo get_template_directory_uri() . '/images/png/clipboard44.png' ;?>">
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Comprehensive Radiation Protection</h4>
+                <p>With a highly qualified and experienced team of UK Health Care Professions Council Registered Clinical Scientists and Technologists, we offer a comprehensive and full-featured protection service</p>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div class="col-md-3 col-sm-6">
+             <div class="media">
+              <div class="media-left">
+                <a href="#" data-toggle="modal" data-target="#myModal">
+                   <img width="84" src="<?php echo get_template_directory_uri() . '/images/png/portfolio8.png' ;?>">  
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Quality Assurance</h4>
+                <p>Gain access to our experienced and highly qualified staff through our QA service, and make sure your service delivers on its full promise</p>
+              </div>
+            </div>
+          </div>
+
+           <div class="col-md-3 col-sm-6">
+             <div class="media">
+              <div class="media-left">
+                <a href="#" data-toggle="modal" data-target="#myModal">
+                 <img width="84" src="<?php echo get_template_directory_uri() . '/images/png/open130.png' ;?>"> 
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Corporate Assurance</h4>
+                <p>Let our team support the achievement of your policies, aims and objectives and targets at all levels and ensure rigorious internal controls are in place</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-md-3 col-sm-6">
+            <div class="media">
+              <div class="media-left">
+                <a href="#" data-toggle="modal" data-target="#myModal">
+                   <img width="84" src="<?php echo get_template_directory_uri() . '/images/png/clipboard46.png' ;?>">
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Project Management</h4>
+                <p>With staff experienced in managing and consulting on a range of projects large and small, we can offer your project the support needed to make it a success</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-6">
+             <div class="media">
+              <div class="media-left">
+                <a href="#" data-toggle="modal" data-target="#myModal">
+                  <img width="84" src="<?php echo get_template_directory_uri() . '/images/png/information38.png' ;?>">
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Training &amp; Staff Development</h4>
+                <p>Our highly qualified team can help provide on-site and tailored training catered to your service's needs</p>
+              </div>
+            </div>
+          </div>
+
+           <div class="col-md-3 col-sm-6">
+             <div class="media">
+              <div class="media-left">
+                <a href="#" data-toggle="modal" data-target="#myModal">
+                   <img width="84" src="<?php echo get_template_directory_uri() . '/images/png/title2.png' ;?>">  
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Full Ethics Guidance</h4>
+                <p>With staff actively involved at a national level, we offer an in-depth, forward looking consultancy service on guidance and regulation</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-6">
+             <div class="media">
+              <div class="media-left">
+                <a href="#" data-toggle="modal" data-target="#myModal">
+                   <img width="84" src="<?php echo get_template_directory_uri() . '/images/png/folders3.png' ;?>">
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Additional Services</h4>
+                <p>We also offer a full-range of additional and complementary services. <a href="#" data-toggle="modal" data-target="#myModal">Get in touch with our team to find out more</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <a role="button" href="#" class="btn btn-lg btn-info btn-block btn-call-out hidden-md hidden-lg" data-toggle="modal" data-target="#myModal">Contact us</a>
+        </div>
+      </div>
+    </div>
+  </div>
